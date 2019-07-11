a1005cc11ae53075f76cfb447128690940948b13
Yash Capoor <yash.capoor@svam.com>
2019-07-11 23:19:57 +0530

    Starting lesson. Adding controllers.

---
+++ csproj_msbuild_core2/start-end/Library/src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Controllers
+{
+    using Library.API.Services;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AuthorsController : Controller
+    {
+
+        private ILibraryRepository libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            this.libraryRepository = libraryRepository;
+        }
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = this.libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
